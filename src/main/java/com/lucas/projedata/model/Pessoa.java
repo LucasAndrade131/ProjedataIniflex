efe94943707eb513e7cf13ad918278fed375cacb
Lucas Andrade <130401115+LucasAndrade131@users.noreply.github.com>
2026-04-08 13:20:56 -0300

    feat: create Pessoa and Funcionario classes with inheritance and core attributes

---
+++ src/main/java/com/lucas/projedata/model/Pessoa.java
@@ -0,0 +1,43 @@
+package com.lucas.projedata.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Pessoa {
+
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(nome, pessoa.nome) && Objects.equals(dataNascimento, pessoa.dataNascimento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, dataNascimento);
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                '}';
+    }
+}
