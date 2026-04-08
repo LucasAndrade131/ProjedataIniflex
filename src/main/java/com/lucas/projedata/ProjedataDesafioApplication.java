3bf235344d719e25979af8062069bf7c530df3b2
Lucas Andrade <130401115+LucasAndrade131@users.noreply.github.com>
2026-04-08 12:46:12 -0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/lucas/projedata/ProjedataDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.projedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjedataDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjedataDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/projedata/ProjedataDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.projedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjedataDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
